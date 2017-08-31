99d8fca794914ea78529dcf560d39c1e760d0d8e
Harry Billinis <Tiltorito@users.noreply.github.com>
2017-08-31 03:53:27 +0300

    Added custom toolbar

--- MyApplication/app/src/main/java/com/example/chessclock/MainActivity.java
+++ MyApplication/app/src/main/java/com/example/chessclock/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.chessclock;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
@@ -19,6 +20,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Toolbar toolbar = (Toolbar)findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
         listView = (ListView)findViewById(R.id.listView);
 
         list.add(new ChessMode("Fischer rapid", 10, 0 ,5));
